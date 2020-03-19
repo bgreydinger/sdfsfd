24a78fea69a9deb1551ad3056e7079b99b9fe230
Max Tretikov <octopirateyt@gmail.com>
2020-03-18 22:37:25 -0700

    bug fix

--- coronahelp/app.py
+++ coronahelp/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask import Flask, url_for, render_template, redirect
-from forms import ContactForm
+# from forms import ContactForm
 
 app = Flask(__name__, instance_relative_config=False)
 app.config.from_object('config.Config')
@@ -10,12 +10,12 @@ app.config.from_object('config.Config')
 def home():
     return render_template('home.html')
 
-@app.route('/', methods=('GET', 'POST'))
-def contact():
-    form = ContactForm()
-    if form.validate_on_submit():
-        return redirect(url_for('success'))
-    return render_template('home.html', form=form, template="form-template")
+# @app.route('/', methods=('GET', 'POST'))
+# def contact():
+#     form = ContactForm()
+#     if form.validate_on_submit():
+#         return redirect(url_for('success'))
+#     return render_template('home.html', form=form, template="form-template")
 
 if __name__ == '__main__':
     app.run(debug=True)
